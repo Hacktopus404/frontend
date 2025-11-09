1221cba5d163b91fb33576950746109504ac0d9c
chukka jagan mohan <jaganchukka567@gmail.com>
2025-11-09 10:16:00 +0530

    Add files via upload

---
+++ Quiz_Web/JaganMohan/quiz.js
@@ -0,0 +1,24 @@
+let box1 = document.querySelector("#b1");
+let box2 = document.querySelector("#b2");
+let box3 = document.querySelector("#b3");
+let box4 = document.querySelector("#b4");
+let box5 = document.querySelector("#b5");
+let box6 = document.querySelector("#b6");
+box1.addEventListener("click",()=>{
+    window.open("quiz2.html","_self");
+});
+box2.addEventListener("click",()=>{
+    window.open("quiz1.html","_self");
+});
+box3.addEventListener("click",()=>{
+    window.open("quiz3.html","_self");
+});
+box4.addEventListener("click",()=>{
+    window.open("quiz4.html","_self");
+});
+box5.addEventListener("click",()=>{
+    window.open("quiz5.html","_self");
+});
+box6.addEventListener("click",()=>{
+    window.open("quiz6.html","_self");
+});
